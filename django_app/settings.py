967a0e7a8cb80f94cd997091599dc1ef0ec1cdb7
Evaldas <emelianas@gmail.com>
2023-07-03 13:39:26 +0300

    Created login, registration and logout handling

--- django_app/settings.py
+++ django_app/settings.py
@@ -130,6 +130,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = 'webble:home'
 LOGIN_URL = '/login'
 LOGOUT_REDIRECT_URL = '/login'
